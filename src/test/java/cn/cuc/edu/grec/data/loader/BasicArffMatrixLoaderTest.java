9aa1f947a4bd03b04679bee380c075dd8dbbb750
kate <18810809501@163.com>
2020-02-27 20:50:22 +0800

    delete useless import

--- src/test/java/cn/cuc/edu/grec/data/loader/BasicArffMatrixLoaderTest.java
+++ src/test/java/cn/cuc/edu/grec/data/loader/BasicArffMatrixLoaderTest.java
@@ -1,15 +1,15 @@
 package cn.cuc.edu.grec.data.loader;
 
 import cn.cuc.grec.data.loader.BasicArffMatrixLoader;
-import cn.cuc.grec.data.loader.BasicCsvMatrixLoader;
 import cn.cuc.grec.data.loader.Loadable;
 import cn.cuc.grec.math.structure.DataSet;
 
-import java.io.IOException;
 
 public class BasicArffMatrixLoaderTest  {
     public static void main(String[] args) throws Exception {
-        Loadable loader = new BasicArffMatrixLoader("C:\\Users\\18810809501\\Desktop\\2020.02\\arff\\test.arff");
+        String url = "";
+        System.out.println("");
+        Loadable loader = new BasicArffMatrixLoader("C:\\Users\\18810809501\\Desktop\\2020.02.22\\src\\test\\resources\\arff\\BasicArffMatrixLoader.arff");
         DataSet result = loader.load();
         System.out.println(result);
     }
