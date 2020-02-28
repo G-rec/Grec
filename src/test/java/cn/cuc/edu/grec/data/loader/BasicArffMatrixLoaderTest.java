0a85024eb1148d691d2a5ce90a4d68a28fa98d17
kate <18810809501@163.com>
2020-02-28 20:37:28 +0800

    alter path of test

--- src/test/java/cn/cuc/edu/grec/data/loader/BasicArffMatrixLoaderTest.java
+++ src/test/java/cn/cuc/edu/grec/data/loader/BasicArffMatrixLoaderTest.java
@@ -5,11 +5,12 @@ import cn.cuc.grec.data.loader.Loadable;
 import cn.cuc.grec.math.structure.DataSet;
 
 
+
 public class BasicArffMatrixLoaderTest  {
+
     public static void main(String[] args) throws Exception {
         String url = "";
-        System.out.println("");
-        Loadable loader = new BasicArffMatrixLoader("C:\\Users\\18810809501\\Desktop\\2020.02.22\\src\\test\\resources\\arff\\BasicArffMatrixLoader.arff");
+        Loadable loader = new BasicArffMatrixLoader("src/test/resources/arff/BasicArffMatrixLoader.arff");
         DataSet result = loader.load();
         System.out.println(result);
     }
