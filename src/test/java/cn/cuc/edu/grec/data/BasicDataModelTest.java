c49b9957046a2e6b7d37eab22d1e01f160108aec
PIC_Desktop <picpic2013@163.com>
2020-03-02 10:28:15 +0800

    Use @Test to test BasicDataModelTest.

--- src/test/java/cn/cuc/edu/grec/data/BasicDataModelTest.java
+++ src/test/java/cn/cuc/edu/grec/data/BasicDataModelTest.java
@@ -4,9 +4,11 @@ import cn.cuc.grec.data.BasicDataModel;
 import cn.cuc.grec.data.DataModel;
 import cn.cuc.grec.data.converter.BasicConverter;
 import cn.cuc.grec.math.structure.BasicDenseMatrix;
+import org.junit.Test;
 
 public class BasicDataModelTest {
-    public static void main(String[] args) {
+    @Test
+    public void demo0() {
         double[][] data = {
                 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
                 {4, 5, 6},
